083875ebaf6c2449ac24282f218907a8fa70b666
Tom Yang <tom@proactiveailab.com>
2026-05-08 10:23:47 -0400

    feat: VC-001 - Create VoiceCommandParser with command lookup table

---
+++ Sources/iGest/Speech/VoiceCommandParser.swift
@@ -0,0 +1,46 @@
+import Foundation
+import Carbon.HIToolbox
+
+enum VoiceCommandResult {
+    case command(GestureAction, wordCount: Int)
+    case text
+}
+
+enum VoiceCommandParser {
+    private static let pressCommands: [String: UInt16] = [
+        "up": UInt16(kVK_UpArrow),
+        "down": UInt16(kVK_DownArrow),
+        "left": UInt16(kVK_LeftArrow),
+        "right": UInt16(kVK_RightArrow),
+        "delete": UInt16(kVK_Delete),
+        "enter": UInt16(kVK_Return),
+        "tab": UInt16(kVK_Tab),
+        "escape": UInt16(kVK_Escape),
+    ]
+
+    private static let commandKeys: [String: UInt16] = [
+        "tab": UInt16(kVK_Tab),
+        "z": 6,
+        "c": 8,
+        "v": 9,
+        "a": 0,
+    ]
+
+    static func parse(newText: String) -> VoiceCommandResult {
+        let words = newText.split(separator: " ", omittingEmptySubsequences: true)
+        guard words.count >= 2 else { return .text }
+
+        let prefix = words[words.count - 2].lowercased()
+        let keyword = words[words.count - 1].lowercased()
+
+        if prefix == "press", let keyCode = pressCommands[keyword] {
+            return .command(.pressKey(keyCode), wordCount: 2)
+        }
+
+        if prefix == "command", let keyCode = commandKeys[keyword] {
+            return .command(.pressModifiedKey(keyCode, shift: false, control: false, option: false, command: true), wordCount: 2)
+        }
+
+        return .text
+    }
+}

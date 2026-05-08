c1f85879848057f2e909381b0c4251cfae088e76
Tom Yang <tom@proactiveailab.com>
2026-05-07 23:03:43 -0400

    fix: revert isThumbPinky to Y-based check (distance-based broke delete)

--- Sources/iGest/Tracking/GestureClassifier.swift
+++ Sources/iGest/Tracking/GestureClassifier.swift
@@ -61,12 +61,8 @@ enum GestureClassifier {
               thumbTip.confidence > 0.3, littleTip.confidence > 0.3 else { return false }
 
         let pinkyExtended = littleTip.location.y > littlePIP.location.y
-        let indexLength = hypot(indexTip.location.x - indexPIP.location.x,
-                                indexTip.location.y - indexPIP.location.y)
-        let middleLength = hypot(middleTip.location.x - middlePIP.location.x,
-                                 middleTip.location.y - middlePIP.location.y)
-        let indexClosed = indexLength < 0.04
-        let middleClosed = middleLength < 0.04
+        let indexClosed = indexTip.location.y <= indexPIP.location.y
+        let middleClosed = middleTip.location.y <= middlePIP.location.y
         let thumbExtended = hypot(thumbTip.location.x - thumbIP.location.x,
                                   thumbTip.location.y - thumbIP.location.y) > 0.03
 
